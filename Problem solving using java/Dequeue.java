c38392e849d2cdfa14c5be82acd3f59271aa33b3
Sanjay V S <vtu27936@veltech.edu.in>
2026-09-16 09:40:26 +0530

    Add files via upload

---
+++ Problem solving using java/Dequeue.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Dequeue
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
+
+        int maxUnique = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int num = sc.nextInt();
+
+            deque.addLast(num);
+            set.add(num);
+
+            if (deque.size() > k)
+            {
+                int removed = deque.removeFirst();
+                
+                if (!deque.contains(removed))
+                {
+                    set.remove(removed);
+
+                }
+            }
+            if (deque.size() == k)
+            {
+                maxUnique = Math.max(maxUnique, set.size());
+            }
+            }
+            System.out.println(maxUnique);
+
+            sc.close();
+
+        }
+
+    }
